6d52ebd3d5e670a3dffcdf97424f3c176e2a27e1
Nabeel <urs.nabeel1@gmail.com>
2025-07-14 22:32:13 +0530

    Added authentication method

--- src/main/java/com/example/finance/banking/controller/HomeController.java
+++ src/main/java/com/example/finance/banking/controller/HomeController.java
@@ -1,13 +1,32 @@
 package com.example.finance.banking.controller;
 
+import com.example.finance.banking.util.UserDetailUtil;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    private final UserDetailUtil util;
+
+    public HomeController(UserDetailUtil util) {
+        this.util = util;
+    }
+
     @GetMapping("/")
     public String get()
     {
         return "welcome";
+
+    }
+    @GetMapping("/authentication")
+    public ResponseEntity<?> checkAuth() {
+         if(util.getUser()!=null)
+         {
+             return ResponseEntity.ok("Authorized");
+         }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Not authorized");
     }
 }
